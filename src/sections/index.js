68bc313037d1619faccc023b01198c8c18b7f24e
job-droid <eyobabebe143sa@gmail.com>
2024-08-25 12:35:31 +0300

    preparing the skelton and imported to the desire destination

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  PopularProduct,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Footer,
+  Subscribe,
+};
