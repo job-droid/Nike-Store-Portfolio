8da182f2732c2c91c43f2adbf14b940194b27dbd
job-droid <eyobabebe143sa@gmail.com>
2024-10-20 15:48:17 +0300

    finshied making the service section, including the service card component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Button({ label, iconURL }) {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red hover:bg-white hover:text-coral-red">
       {label}
-      <img src={iconURL} alt="button" className="ml-2 rounded-full w-5 h-5" />
+      {iconURL && (
+        <img src={iconURL} alt="button" className="ml-2 rounded-full w-5 h-5" />
+      )}
     </button>
   );
 }
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,23 +1,36 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
 
-const SuperQuality = () => {
+function SuperQuality() {
   return (
-    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
       <div className="flex flex-1 flex-col">
-        <h2 className="mt-10 font-palanquin text-4xl capitalize max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="xl:whitespace-normal relative z-10 pr-10">
-            The New Arrival
-          </span>
-          <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You <br />
+          <span className="text-coral-red">Super</span>
+          <span className="text-coral-red ml-2">Quality</span> Shoes
         </h2>
-        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover Stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
-        <Button label="View Details"/>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and execellence ensures your satsfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+
+      <div className="flex flex-1 justify-center items-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain"/>
       </div>
     </section>
-  )
+  );
 }
 
-export default SuperQuality
+export default SuperQuality;
