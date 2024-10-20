5ef225d58c8a239d05754a7924bb3e93f32717b8
job-droid <eyobabebe143sa@gmail.com>
2024-10-20 16:20:55 +0300

    done special offers section

--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { offer } from '../assets/images'
+import Button from '../components/Button'
+import { arrowRight } from '../assets/icons'
 
-function SpecialOffers() {
+const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container'>
+        <div className='flex-1'>
+            <img src={offer} width={773} height={687} className='object-contain w-full' alt='offer'/>
+        </div>
+        <div className="flex flex-1 flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">Special </span>
+          offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and execellence ensures your satsfaction
+        </p>
+        <div className="mt-11 flex flex-wrap gap-5">
+          <Button label="Shop Now" iconURL={arrowRight}/>
+          <Button label="Learn more" 
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
