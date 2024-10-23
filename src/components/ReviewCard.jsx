f40f8f6baa931a524c329742d19d43d6830c09f5
job-droid <eyobabebe143sa@gmail.com>
2024-10-23 17:11:42 +0300

    finished footer section and made some adjustments in reviewCard, serviceCard and customerReview

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { star } from "../assets/icons";
 
-const ReviewCard = ({ imageURL, customerName, rating, feedback }) => {
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
   return (
     <div className="flex justify-center items-center flex-col">
       <img
-        src={imageURL}
+        src={imgURL}
         alt="customer"
         className="rounded-full object-cover w-[120px] h-[120px]"
       />
@@ -13,12 +13,12 @@ const ReviewCard = ({ imageURL, customerName, rating, feedback }) => {
       <div className="mt-3 flex justify-center items-center gap-2.5">
         <img
           src={star}
-          alt="star"
           width={24}
           height={24}
+          alt="rating star"
           className="object-contain m-0"
         />
-        <p className="text-xl font-montserrat text-slate-gray">{rating}</p>
+        <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
       </div>
       <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">
         {customerName}
